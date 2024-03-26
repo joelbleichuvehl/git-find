53398b2593e55507c6f1b75eef5bcd4e9cc58626
Joel Bleichuvehl <joel@cetesc.com.br>
2024-03-26 20:58:57 -0300

    first commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,86 @@
+import Header from "../../components/Header";
+import Background from "../../assets/background.png";
+import "./styles.css";
+import ItemList from "../../components/ItemList";
+import { useState } from "react";
+
+const Home = () => {
+  const [user, setUser] = useState("");
+  const [currentUser, setCurretnUser] = useState("");
+  const [repos, setRepos] = useState([]);
+
+  const handleGetData = async () => {
+    console.log(user);
+    const userData = await fetch(`https://api.github.com/users/${user}`);
+    const newUser = await userData.json();
+
+    if (newUser) {
+      console.log(newUser);
+      const { avatar_url, name, bio, login } = newUser;
+      setCurretnUser({ avatar_url, name, bio, login });
+
+      const reposData = await fetch(
+        `https://api.github.com/users/${user}/repos`
+      );
+      const newRepos = await reposData.json();
+
+      if (newRepos.length) {
+        setRepos(newRepos);
+      }
+    }
+  };
+
+  return (
+    <div className="app">
+      <Header />
+
+      <div className="conteudo">
+        <img className="background" src={Background} alt="Imagem Git" />
+
+        <div className="info">
+          <div>
+            <input
+              type="text"
+              name="usuario"
+              value={user}
+              onChange={(e) => setUser(e.target.value)}
+              placeholder="@username"
+            />
+            <button onClick={handleGetData}>Buscar</button>
+          </div>
+
+          <>
+            <div className="perfil">
+              <img
+                className="profile"
+                src={currentUser.avatar_url}
+                alt="imagem perfil"
+              />
+
+              <div>
+                <h3>{currentUser.name || "Usuário do GitHub"}</h3>
+                <span>@{currentUser.login}</span>
+                <p>{currentUser.bio}</p>
+              </div>
+            </div>
+            <hr />
+          </>
+
+          <div>
+            <h4 className="repositorio">Repositórios</h4>
+            <span>{repos.length} repositórios</span>
+            {repos.map((repo, index) => (
+              <ItemList
+                key={index}
+                title={repo.name}
+                description={repo.description}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
